cd95e5668593ec15f951548487e38c61f32610d7
Daniel Thomas <63409207+DanielTk26@users.noreply.github.com>
2021-09-04 12:31:20 +0300

    Update BrowserUnsupported.js

--- src/components/BrowserUnsupported/BrowserUnsupported.js
+++ src/components/BrowserUnsupported/BrowserUnsupported.js
@@ -4,10 +4,10 @@ import './BrowserUnsupported.css';
 export default function BrowserUnsupported() {
   return (
     <p class="browser-unsupported">
-      Looks like you need to upgrade your browser to make Daily video calls.
+      Looks like your browser does not support Online octor Consultation App Video Call hosted by netlify and running on daily.co video calling api
       <br />
       See&nbsp;
-      <a href="https://docs.daily.co/docs/browsers">this page</a>
+      <a href="https://docs.daily.co/docs/browsers">the official api page</a>
       &nbsp;for help getting on a supported browser version.
     </p>
   );
