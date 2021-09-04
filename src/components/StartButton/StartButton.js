7ee302525f498a03fdef4d9f855865f6057981e0
Daniel Thomas <63409207+DanielTk26@users.noreply.github.com>
2021-09-04 12:34:05 +0300

    Update StartButton.js

--- src/components/StartButton/StartButton.js
+++ src/components/StartButton/StartButton.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import './StartButton.css';
 
-/**
- * Props:
- * - disabled: boolean
- * - onClick: () => ()
- */
 export default function StartButton(props) {
   return (
     <button
@@ -13,7 +8,7 @@ export default function StartButton(props) {
       disabled={props.disabled}
       onClick={props.onClick}
     >
-      Click to start a call
+      Click to start the call
     </button>
   );
 }
